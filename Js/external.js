50c534080b8050082c86361525b0f7deba629a60
Kristen Wilson <wilson.kristen614@gmail.com>
2021-07-06 13:55:41 -0500

    Made inline/external js files and a js folder w? external.js did html ex, now need to do the datatype ex

--- Js/external.js
+++ Js/external.js
@@ -1,1 +1,14 @@
-console.log("hello from external Javascript")
+
+
+
+
+
+alert("Welcome to my website!");
+
+console.log("hello from external Javascript");
+var favColor = prompt("what's your favorite color?");
+
+alert(favColor + "Wow thats my favorite too");
+console.log('The user entered: ' + favColor);
+
+//
