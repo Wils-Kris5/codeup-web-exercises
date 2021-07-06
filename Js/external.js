d5f6231ecfc7b356abcc60c49d76b9df4cbcc2ce
Kristen Wilson <wilson.kristen614@gmail.com>
2021-07-06 14:40:19 -0500

    Made inline/external js files, external.js and a js folder w

--- Js/external.js
+++ Js/external.js
@@ -1,3 +1,6 @@
+"use strict";
+
+
 alert("Welcome to my website!");
 
 console.log("hello from external Javascript");
@@ -5,4 +8,16 @@ var favColor = prompt("what's your favorite color?");
 
 alert(favColor + "Wow thats my favorite too");
 console.log('The user entered: ' + favColor);
+
+// Movie Rentals
+var pricperDay = 3;
+var littleMermDays = prompt("How many days will you be renting the Little Mermaid?");
+var broBearDays = prompt("How many days will you be renting Brother Bear");
+var hercDays = prompt("How many days will you be renting Hercules?");
+var totalRentalCost = (littleMermDays * 3) + (broBearDays * 3) + (hercDays * 3);
+var totalRentalCost = alert("Your total will be $" +
+    (parseInt(littleMermDays) + parseInt(broBearDays) + parseInt(hercDays)
+
+
+
 
