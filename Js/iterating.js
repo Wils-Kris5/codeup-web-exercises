a8cbbdf0af7174d9c141941e14c354d380cb0513
Kristen Wilson <wilson.kristen614@gmail.com>
2021-07-18 11:18:58 -0500

    All exercises completegit status

--- Js/iterating.js
+++ Js/iterating.js
@@ -37,19 +37,19 @@
 	// }
 //This is Roxanne
 // VM276:2 This is Christopher
-// VM276:2 This is friend Kimberly
-// VM276:2 This is friend Johnny
+// VM276:2 This is Kimberly
+// VM276:2 This is Johnny
 // undefined
 	/**
 	 * TODO:
 	 * Refactor your above code to use a `forEach` loop
 	 */
-	names.forEach(function(element, index, array){
-
-		console.log("Element " + element + " is at the index " + index);
-		console.log(index);
-		console.log(array);
-	});
+	// names.forEach(function(element, index, array){
+	//
+	// 	console.log("Element " + element + " is at the index " + index);
+	// 	console.log(index);
+	// 	console.log(array);
+	// });
 
 	// Element Roxanne is at the index 0
 	// VM660:4 0
@@ -68,8 +68,10 @@
 
 	/**
 	 * TODO:
-	 * Create the following three functions, each will accept an array and
+	 * Create the following three functions,
+	 * each will accept an array and
 	 * return an an element from it
+
 	 * - first: returns the first item in the array
 	 * - second: returns the second item in the array
 	 * - last: returns the last item in the array
@@ -77,30 +79,27 @@
 
 	 * Example:
 	 */
+	//
+	// var theArray = [1, 2, 3, 4, 5];
+    // var index = theArray.indexOf(1);
+    // console.log(index);
+    // var theLength = theArray.length
+	// console.log(theLength); // 5
 
-	const theArray = [1, 2, 3, 4, 5];
-	const eleMents = [0, 1, 2, 3, 4];
-	theArray.push.apply(theArray, eleMents);
 
-	const  = math.max
 	// second([1, 2, 3, 4, 5]) // returns 2
 	//  last([1, 2, 3, 4, 5]) // return 5
 	//
-function first() {
-
-	}
-function second(seq) {
-
-
-	var x = [ 'p0', 'p1', 'p2' ];
-	call_me(x);
-
-	function call_me(params) {
-		for (i=0; i<params.length; i++) {
-			alert(params[i])
-		}
-	}
-
+// function first(input) {
+//    return theArray[0];
+// }
+//
+// function second(input) {
+// 	return theArray[1];
+// }
+// function last(input) {
+// 	return theArray[4];
+// }
 
 
 
