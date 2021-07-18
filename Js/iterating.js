aa71fed5a7627b5f0d08b6c6ece0a6932739e451
Kristen Wilson <wilson.kristen614@gmail.com>
2021-07-17 22:27:18 -0500

    last Exercise almost finished xo

--- Js/iterating.js
+++ Js/iterating.js
@@ -1,61 +1,109 @@
 (function(){
-    "use strict";
+	"use strict";
 
     /**
      * TODO:
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-    var names = ['Roxanne', 'Christopher', 'Kimberly', 'Johnny'];
+    var names = ["Roxanne", "Christopher", "Kimberly", "Johnny"];
 
-    /**
-     * TODO:
-     * Create a log statement that will log the number of elements in the names
-     * array.
-     */
-    console.log(names.length); // 4
-    /**
-     * TODO:
-     * Create log statements that will print each of the names individually by
-     * accessing each element's index.
-     */
+	/**
+	 * TODO:
+	 * Create a log statement that will log the number of elements in the
+	 * names
+	 * array.
+	 */
+    // var numOfNames = names.length;
+	// console.log(numOfNames); // 4
+	/**
+	 * TODO:
+	 * Create log statements that will print each of the names individually by
+	 * accessing each element's index.
+	 */
+    //
+	// console.log("This is " + names[0]);  // This is Roxanne
+	// console.log("This is " + names[1]);  // This is Christopher
+	// console.log("This is " + names[2]);  // This is Kimberly
+	// console.log("This is " + names[3]); // This is Johnny
 
-    console.log(names[0])   // 1
-    console.log(names[1]); // 2
-    console.log(names[2]); // 3
-    console.log(names[3]); // 4
-    /**
-     * TODO:
-     * Write some code that uses a for loop to log every item in the names
-     * array.
-     */
-    for(var i = 0; i < names.length; i += 1) {
-        console.log('This is my friend ' + names[i]);
-    }
-//This is my friend Roxanne
-// VM276:2 This is my friend Christopher
-// VM276:2 This is my friend Kimberly
-// VM276:2 This is my friend Johnny
+	/**
+	 * TODO:
+	 * Write some code that uses a for loop to log every item in the names
+	 * array.
+	 */
+	// for(var i = 0; i < names.length; i += 1) {
+	// 	console.log("This is " + names[i]);
+	// }
+//This is Roxanne
+// VM276:2 This is Christopher
+// VM276:2 This is friend Kimberly
+// VM276:2 This is friend Johnny
 // undefined
-    /**
-     * TODO:
-     * Refactor your above code to use a `forEach` loop
-     */
-    names.forEach(function(elements) {
+	/**
+	 * TODO:
+	 * Refactor your above code to use a `forEach` loop
+	 */
+	names.forEach(function(element, index, array){
 
-    })
-    /**
-     * TODO:
-     * Create the following three functions, each will accept an array and
-     * return an an element from it
-     * - first: returns the first item in the array
-     * - second: returns the second item in the array
-     * - last: returns the last item in the array
-     *
-     * Example:
-     *  > first([1, 2, 3, 4, 5]) // returns 1
-     *  > second([1, 2, 3, 4, 5]) // returns 2
-     *  > last([1, 2, 3, 4, 5]) // return 5
-     */
+		console.log("Element " + element + " is at the index " + index);
+		console.log(index);
+		console.log(array);
+	});
 
-})();
+	// Element Roxanne is at the index 0
+	// VM660:4 0
+	// VM660:5 (4) ["Roxanne", "Christopher", "Kimberly", "Johnny"]
+	// VM660:3 Element Christopher is at the index 1
+	// VM660:4 1
+	// VM660:5 (4) ["Roxanne", "Christopher", "Kimberly", "Johnny"]
+	// VM660:3 Element Kimberly is at the index 2
+	// VM660:4 2
+	// VM660:5 (4) ["Roxanne", "Christopher", "Kimberly", "Johnny"]
+	// VM660:3 Element Johnny is at the index 3
+	// VM660:4 3
+	// VM660:5 (4) ["Roxanne", "Christopher", "Kimberly", "Johnny"]
+	// undefined
+
+
+	/**
+	 * TODO:
+	 * Create the following three functions, each will accept an array and
+	 * return an an element from it
+	 * - first: returns the first item in the array
+	 * - second: returns the second item in the array
+	 * - last: returns the last item in the array
+	 *
+
+	 * Example:
+	 */
+
+	const theArray = [1, 2, 3, 4, 5];
+	const eleMents = [0, 1, 2, 3, 4];
+	theArray.push.apply(theArray, eleMents);
+
+	const  = math.max
+	// second([1, 2, 3, 4, 5]) // returns 2
+	//  last([1, 2, 3, 4, 5]) // return 5
+	//
+function first() {
+
+	}
+function second(seq) {
+
+
+	var x = [ 'p0', 'p1', 'p2' ];
+	call_me(x);
+
+	function call_me(params) {
+		for (i=0; i<params.length; i++) {
+			alert(params[i])
+		}
+	}
+
+
+
+
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Function/apply
+})()
