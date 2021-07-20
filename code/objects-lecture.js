4e14f3472357cd72dd5236dd0518fce2b58d6034
Kristen Wilson <wilson.kristen614@gmail.com>
2021-07-20 13:46:20 -0500

    left off at shoppers

--- code/objects-lecture.js
+++ code/objects-lecture.js
@@ -32,15 +32,15 @@ var currentWeather = {
 currentWeather.uvIndex = 9.79;
 
 // loop through the object and get the property names
-for(var prop in currentWeather) {
-	console.log(prop + ": " + currentWeather[prop]);
-}
+// for (var prop in currentWeather){
+//     console.log(prop + ": " + currentWeather[prop]);
+// }
 
-console.log("The UV index is " + currentWeather.uvIndex);
+// console.log("The UV index is " + currentWeather.uvIndex);
 
 // you can delete properties from an object
-delete currentWeather.uvIndex;
-console.log("The UV index is " + currentWeather.uvIndex);
+// delete currentWeather.uvIndex;
+// console.log("The UV index is " + currentWeather.uvIndex);
 
 var hourlyWeather = [
 	{
@@ -64,67 +64,193 @@ var hourlyWeather = [
 		feels_like: 89.24
 	}
 ];
-console.log(hourlyWeather[0].temp);
-//>combining array and object using dot notation
-// to access the elements of the object
-// looping through the array for loop & forEach
-/* var i = 0; i < hourlyWeather.length; i++){ console.log("At " + "the temperature will be " + hourlyWeather[i].temp +
-+ "and it feels like " + hourlyWeather[i].feels_like);
-}
-hourlyWeather.forEach(function(hourlyForcast) {
-console.log("At " + hourlyForecast.time + " the temp is " + hourlyForecast.temp + " and feels like " + hourlyFOrecast.feels_like);
-});
-	  __>>> insert houston notes example
- */
-/*
-========METHODS:
-*/
-// this is keyword
-var fighter = {
-	// state: what the objects knows about itself
-	name: "Arata",
-	hitPoints: 18,
-	maxDamage: 8,
-	/*behavior: what the obj does
-	this is a method, a method is a function inside an object.
-	 */
-	attack: function (target) {
-		console.log(this.name + " attacks" + " this " + target.name + "!");
-		console.log(target.name + " has " + target.hitPoints + " hit points.");
-		var damage = Math.ceil(Math.random() * this.maxDamage);
-		console.log(this.name + " does" + damage + " point of damage!");
-		// lets have an object's behavior affect another objects state
-		targe.hitPoints -= damage;
-		console.log(target.name + "now has " + target.hitPoints + " hit points left.");
 
+// console.log(hourlyWeather[0].temp);
+
+// for (var i = 0; i < hourlyWeather.length; i++){
+//     console.log("Object number " + i + " At " + hourlyWeather[i].time + " the temperature will be " + hourlyWeather[i].temp + " and feel like " + hourlyWeather[i].feels_like);
+// }
+
+// hourlyWeather.forEach(function(hourlyForecast){
+//     console.log("At " + hourlyForecast.time + " the temperture will be " + hourlyForecast.temp + " and feel like " + hourlyForecast.feels_like);
+// });
+
+var sanAntonio = {
+	latitude: 29.423017,
+	longitude: -98.48527,
+	timezone: "America/Chicago",
+	currentWeather: {
+		humidity: 77,
+		temp: 82.33,
+		feels_like: 88.79,
+		clouds: 75
 	}
 }
+
+// console.log("The San Antonio time zone is " + sanAntonio.timezone + " and the current humidity is " + sanAntonio.currentWeather.humidity);
+
+var texasInfo = [
+	{
+		city: "San Antonio",
+		latitude: 29.423017,
+		longitude: -98.48527,
+		timezone: "America/Chicago",
+		currentWeather: {
+			humidity: 77,
+			temp: 82.33,
+			feels_like: 88.79,
+			clouds: 75
+		}
+	},
+	{
+		city: "Houston",
+		latitude: 29.7915,
+		longitude: -95.093,
+		timezone: "America/Chicago",
+		currentWeather: {
+			humidity: 68,
+			temp: 87.44,
+			feels_like: 97.7,
+			clouds: 75
+		}
+	}
+]
+
+// console.log("The temperature in " + texasInfo[1].city + " is " + texasInfo[1].currentWeather.temp);
+
+// var fighter = {
+//     // state: what the object knows about itself
+//     name: "Arata",
+//     hitPoints: 18,
+//     maxDamage: 8,
+//     // behavior: what the object does
+//     // this is a method
+//     // a method is a function inside an object
+//     attack: function(target){
+//         console.log(this.name + " attacks" + " the " + target.name + "!");
+//         console.log(target.name + " has " + target.hitPoints + " hit points.");
+//         var damage = Math.ceil(Math.random() * this.maxDamage);
+//         console.log(this.name + " does " + damage + " points of damage!");
+//         // let's have an object's behavior affect another object's state
+//         target.hitPoints -= damage;
+//         console.log(target.name + " now has " + target.hitPoints + " hit points left.");
+//     }
+// }
+
+// the this keyword
+
+// var monster = {
+//     name: "Goblin",
+//     hitPoints: 8,
+//     maxDamage: 6,
+// }
+//
+// // fighter.attack(monster);
+//
+// var fighter = {
+//     // state: what the object knows about itself
+//     name: "Arata",
+//     hitPoints: 18,
+//     maxDamage: 8,
+//     attack: function(){
+//         console.log(this.name + " attacks!");
+//     }
+// }
+
+// monster.attack();
+
+// var controller = {
+//     attack: function(attacker, defender){
+//         console.log(attacker.name + " attacks" + " the " + defender.name + "!");
+//         console.log(defender.name + " has " + defender.hitPoints + " hit points.");
+//         var damage = Math.ceil(Math.random() * attacker.maxDamage);
+//         console.log(attacker.name + " does " + damage + " points of damage!");
+//         // let's have an object's behavior affect another object's state
+//         defender.hitPoints -= damage;
+//         console.log(defender.name + " now has " + defender.hitPoints + " hit points left.");
+//         console.log("------------------------");
+//     }
+// }
+
+// controller.attack(fighter, monster);
+// controller.attack(monster, fighter);
+//
+// var model = {
+//     monster: {
+//         name: "Goblin",
+//         hitPoints: 8,
+//         maxDamage: 6,
+//     },
+//     fighter: {
+//         name: "Arata",
+//         hitPoints: 18,
+//         maxDamage: 8,
+//     }
+// }
+//
+// var controller = {
+//     attack: function(attacker, defender){
+//         var hitPointsBeforeAttack = defender.hitPoints;
+//         var damage = Math.ceil(Math.random() * attacker.maxDamage);
+//         defender.hitPoints -= damage;
+//         view.displayAttackResults(attacker.name, defender.name, damage, hitPointsBeforeAttack, defender.hitPoints);
+//     }
+// }
+//
+//
+// var view = {
+//     displayAttackResults: function(attackerName, defenderName, damage, defenderHitPoints, defenderHitPointsAfterDamage){
+//         console.log(attackerName + " attacks" + " the " + defenderName + "!");
+//         console.log(defenderName + " has " + defenderHitPoints + " hit points.");
+//         console.log(attackerName + " does " + damage + " points of damage!");
+//         console.log(defenderName + " now has " + defenderHitPointsAfterDamage + " hit points left.");
+//         console.log("------------------------");
+//     }
+// }
+//
+//
+// controller.attack(model.fighter, model.monster);
+// controller.attack(model.monster, model.fighter);
+
+
 var monster = {
 	name: "Goblin",
 	hitPoints: 8,
-	maxDamage: 6,
-	// attack: function(){
-	//     console.log(this.name + "attacks!");
-};
-
-// fighter.attack(monster);
-// monster.attack();
-/*
-
- */
-var controller = {
-    attack: function (attacker, defender){
-        console.log(this.name + " attacks" + " this " + attacker.name + "!");
-        console.log(target.name + " has " + target.hitPoints + " hit points.");
-        var damage = Math.ceil(Math.random() * attacker.maxDamage);
-        console.log(this.name + " does" + damage + " point of damage!");
-        // lets have an object's behavior affect another objects state
-        targe.hitPoints -= damage;
-        console.log(target.name + "now has " + defender.hitPoints + " hit points left.");
-    }
+	maxDamage: 6
 }
 
-controller.attack(fighter, monster);
-controller.attack(monster, fighter);
+var fighter = {
+	name: "Arata",
+	hitPoints: 18,
+	maxDamage: 8
+}
+
+var anotherGoblin = {};
+anotherGoblin.name = "Goblin";
+anotherGoblin.hitPoints = 8;
+anotherGoblin.maxDamage = 6;
 
 
+// We can make a function that returns an object
+function makeAGoblin(name, hitPoints, maxDamage){
+	return {
+		name: name,
+		hitPoints: hitPoints,
+		maxDamage: maxDamage
+	}
+}
+
+var hobgoblin = makeAGoblin("Hobgoblin", 11, 11);
+console.log(hobgoblin.name + " has " + hobgoblin.hitPoints + " hit points.");
+
+
+// ... or we can use an actual object constructor
+// by convention, a constructor function is capitalized
+function Monster(name, hitPoints, maxDamage){
+	this.name = name;
+	this.hitPoints = hitPoints;
+	this.maxDamage = maxDamage;
+}
+
+var hobgoblinCaptain = new Monster("Hobgoblin Captain", 39, 14);
+console.log(hobgoblinCaptain.name + " has " + hobgoblinCaptain.hitPoints + " hit points.");
