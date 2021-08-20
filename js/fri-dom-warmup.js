deb5da70b2eb69c35d039c783d8dfa5bf6799a8b
Kristen Wilson <wilson.kristen614@gmail.com>
2021-08-20 09:37:47 -0500

    just commiting some unfinsih work really want to work on the warmup again later and need to refactor yesterdays code solutions

---
+++ js/fri-dom-warmup.js
@@ -0,0 +1,20 @@
+$("#add-btn").click(function(){
+    $("#number-output").click();
+});
+
+
+
+
+//     var counter = 0;
+//     return {
+//         increment:function(){
+//          counter++;
+//     },
+//         print:function(){
+//             console.log(counter);
+//         }
+//     }
+// }
+// var c = addtoThis();
+// c.increment();
+// c.print();
