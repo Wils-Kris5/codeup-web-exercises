7814b7212147c9379653f6126357078cab87f461
Kristen Wilson <wilson.kristen614@gmail.com>
2021-09-09 19:40:23 -0500

    what i have so far will push promises soon as confirmation

--- js/promises.js
+++ js/promises.js
@@ -4,15 +4,16 @@
 //
 // Generate a Personal Access Token for the GitHub API.
 // We will use this so that we don't get rate limited when talking to the GitHub API. You can add the token to your requests ike this:
+//  Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made.
+//  Reference the github api documentation to achieve this.
 //
-//
-// fetch(url, {headers: {'Authorization': 'token YOUR_TOKEN_HERE'}})to achieve this
+ fetch(url, {headers: {'Authorization': 'GIT_HUB_TOKEN'}});
 
 
 
 
      function lastPushDate(username) {
-         return fetch(`https://api.github.com/users/${username}/events`, {
+         return fetch(`https://api.github.com/users/${wils-kris5}/events`, {
              headers: {
                  'Authorization': 'token' +
                      GIT_HUB_TOKEN
@@ -24,11 +25,10 @@
      }
          console.log(lastPushDate('Wils-Kris5'));
          
-      /* ========     Justins example
 
 
 
 
 
 
-       */
+
